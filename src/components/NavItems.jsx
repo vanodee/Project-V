c8df2e3bb741d86afb254c12281512c8d1ae78fd
vanodee <vanodee007@gmail.com>
2024-09-09 19:30:47 +0100

    Updated CV

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -6,7 +6,7 @@ const links = [
     { id: 1, name: "Home", path: "/" },
     { id: 2, name: "Projects", path: "Projects" },
     { id: 3, name: "About", path: "About" },
-    { id: 4, name: "Resume", path: "https://drive.google.com/file/d/1XbjO0b3Ly7LKs5L9_hhK7UjKA3zmSAFe/view?usp=sharing" },
+    { id: 4, name: "Resume", path: "https://drive.google.com/file/d/1OfDbp4bgZH9xyV2PkK3bwdprr-5S2IeO/view?usp=sharing" },
     { id: 5, name: "Contact", path: "Contact" },
 ];
 
